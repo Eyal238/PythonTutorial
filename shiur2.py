8722fcd0275962ca85906f6ff55439207da29da6
Eyal238 <130982982+Eyal238@users.noreply.github.com>
2023-05-07 19:39:27 +0300

    Update shiur2.py

--- shiur2.py
+++ shiur2.py
@@ -4,3 +4,8 @@ x=1
 y=3
 print(x,end="")
 print(y,end="")
+
+# 6. כתוב תוכנית הקולטת מהמקלדת שם פרטי במשתנה אחד ושם משפחה במשתנה אחר, התוכנית תדפיס באנגלית: "שלום", שם פרטי "ובוקר טוב" שם משפחה
+name1=input("Enter your first name:")
+name2=input("Enter your last name:")
+print("Shalom",name1,"Boker Tov",name2) 
