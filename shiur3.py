5ccac81d611b0b3f6febdd4b105a0bfd8645448c
Eyal238 <130982982+Eyal238@users.noreply.github.com>
2023-05-12 18:46:35 +0300

    Update shiur3.py

--- shiur3.py
+++ shiur3.py
@@ -1,5 +1,5 @@
 # 1.כתוב תוכנית הקולטת מהמקלדת 2 משתנים שלמים, התוכנית תדפיס את מכפלתם.
-x=int(input("enter your num:"))
+x=int(input("Enter your num:"))
 y=int(input("Enter your num:"))
 print(x*y)
 
