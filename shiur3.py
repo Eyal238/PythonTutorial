e8e734d74b2235391d4eebbe18d8532d031c7860
Eyal238 <130982982+Eyal238@users.noreply.github.com>
2023-05-14 08:53:28 +0300

    Update shiur3.py

--- shiur3.py
+++ shiur3.py
@@ -3,4 +3,10 @@ x=int(input("Enter your num:"))
 y=int(input("Enter your num:"))
 print(x*y)
 
+# 5. כתוב תוכנית שבוחרת שני מספרים בין 1 ל-10 באופן רנדומלי ומדפיסה את מכפלתם.
+import random
+x=random.randrange(1,11)
+y=random.randrange(1,11)
+print(x*y)
+
 
