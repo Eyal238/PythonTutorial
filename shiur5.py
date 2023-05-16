9a2acee13c85f640ad927309f5959c1f30a0501c
Eyal238 <130982982+Eyal238@users.noreply.github.com>
2023-05-16 10:39:16 +0300

    Create shiur5.py

---
+++ shiur5.py
@@ -0,0 +1,8 @@
+#   כתוב מחשבון
+print("1 - Add +")
+print("2 - Subtract - ")
+print("3 - Multiplication *")
+print("4 - division /")
+print("5 - Remainder of the division(modulo) %")
+print("6 - Division without remainder //")
+print("7 - Power **")
