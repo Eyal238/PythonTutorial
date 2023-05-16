b557368e21e21da5c6d47e11623c07b75549bcc0
Eyal238 <130982982+Eyal238@users.noreply.github.com>
2023-05-16 07:58:10 +0300

    Update shiur3.py

--- shiur3.py
+++ shiur3.py
@@ -2,7 +2,10 @@
 x=int(input("Enter your num:"))
 y=int(input("Enter your num:"))
 print(x*y)
-
+# 2. כתוב תוכנית המגדירה 2 משתנים עשרוניים, התוכנית תדפיס את ההפרש שלהם.
+x=10.5
+y=5.3
+print(x-y)
 # 5. כתוב תוכנית שבוחרת שני מספרים בין 1 ל-10 באופן רנדומלי ומדפיסה את מכפלתם.
 import random
 x=random.randrange(1,11)
