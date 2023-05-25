c93e764e1eaf26c07611fc34908ac3a86673aaea
Eyal238 <130982982+Eyal238@users.noreply.github.com>
2023-05-25 12:11:25 +0300

    Update shiur5.py

--- shiur5.py
+++ shiur5.py
@@ -14,6 +14,27 @@ def f2(x,y,z):
   else:
     return z
 print(f2(5,3,4))
+# 3.  בחנות גלידות ניתן לקנות קרטיב בשקל ועשרים אגורות. מחיר עשרה קרטיבים הוא 10 שקלים. וקרטון של 54 קרטיבים ניתן לקנות ב-50 שקלים. כך גם אם נקנו מעל 50 אך פחות מ-54. 
+#    לא ניתן לקנות מעל 108 קרטיבים בקנייה אחת. כלומר עד 2 קרטונים. כתוב פונקציה המקבלת את מספר הקרטיבים שנקנו ומדפיסה את המחיר 
+def f3(amount):
+  if amount==1:
+    print(1.2)
+  elif (amount>1)and(amount<10):
+    print(amount*1.2)
+  elif amount==10:
+    print(10)
+  elif (amount>10)and(amount<50):
+    complete=amount//10
+    remainder=amount%10
+    print((complete*10)+(remainder*1.2))
+  elif (amount>=50)and(amount<55):
+    print(50)
+  elif (amount>54)and(amount<108):
+    complete=(amount-54)//10
+    remainder=(amount-54)%10
+    print(50+(complete*10)+(remainder*1.2))
+  elif amount==108:
+    print(100)
 #   כתוב מחשבון
 print("1 - Add +")
 print("2 - Subtract - ")
