6ae62092d8795d2b793c25485a46c94d37bc8592
Eyal238 <130982982+Eyal238@users.noreply.github.com>
2023-05-25 13:42:10 +0300

    Update shiur5.py

--- shiur5.py
+++ shiur5.py
@@ -51,11 +51,3 @@ def f4(amount):
     print(50+(complete*10)+(remainder*1.2))
   elif amount==108:
     print(100)
-#   כתוב מחשבון
-print("1 - Add +")
-print("2 - Subtract - ")
-print("3 - Multiplication *")
-print("4 - division /")
-print("5 - Remainder of the division(modulo) %")
-print("6 - Division without remainder //")
-print("7 - Power **")
