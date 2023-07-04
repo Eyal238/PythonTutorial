12f400f0531b7cd4d3dce56979207d7591ad4913
Eyal238 <130982982+Eyal238@users.noreply.github.com>
2023-07-04 19:04:02 +0300

    Update shiur6.py

--- shiur6.py
+++ shiur6.py
@@ -11,3 +11,23 @@ def f1():
          sum=sum+grade
     ave=round(sum/7,3)
     print("The average is:",ave)
+
+# 2. כתוב פונקציה הקולטת 5 ציונים בין 0 ל- 100 מהמקלדת, על הפונקציה לבדוק תקינות קלט ולהדפיס את הציון המקסימלי והמינימלי מתוך הציונים שנקלטו.
+def f2():
+    i=0
+    min=100
+    max=0
+    while i<5:
+       x=int(input("Enter your score:"))
+       if (x>=0)and(x<=100):
+           if x<min:
+               min=x
+           if x>max:
+               max=x
+           i=i+1
+       else:
+           print("Sorry.is not a score. ")
+    print("The max grade is:",max)
+    print("The min grade is:",min)
+
+
