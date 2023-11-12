89901d15a0d73f1476c2ebdd7624bfb0b19da57d
Eyal238 <130982982+Eyal238@users.noreply.github.com>
2023-11-12 09:56:30 +0200

    Update shiur2.py

--- shiur2.py
+++ shiur2.py
@@ -4,6 +4,11 @@ num2=2.5
 num3=4
 print(num1,num2,num3)
 
+# 2.
+str1="Proud to live in israel"
+str2="and happy to study Computer Science"
+print(str1,str2)
+
 # 5. כתוב תוכנית הקולטת 2 מספרים שלמים המדפיסה אותם עם רווח בודד
 x=1
 y=3
