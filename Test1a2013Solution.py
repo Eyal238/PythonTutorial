0a09630d973db99ef96afa45f7c2a17042fd3a13
Eyal238 <130982982+Eyal238@users.noreply.github.com>
2023-11-29 12:45:11 +0200

    Update Test1a2013Solution.py

--- Test1a2013Solution.py
+++ Test1a2013Solution.py
@@ -8,5 +8,15 @@ def f1(r):
     print(round(S,3))
     print(round(P,3))
 #2.
-
+def f2():
+    count=0
+    num=int(input("Enter your number:"))
+    if(num%2==0):
+        print("even.")
+    else:
+        print("odd.")
+    while(num>0):
+        count+=1
+        num=num//10
+    print(count)
     
