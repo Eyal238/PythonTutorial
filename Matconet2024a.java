8b267fb52bc6a6eed017e22aa2d62e845df9d1cc
Eyal238 <130982982+Eyal238@users.noreply.github.com>
2024-03-14 10:59:29 +0200

    Create Matconet2024a.java

---
+++ Matconet2024a.java
@@ -0,0 +1,14 @@
+def f1():
+    x1=random.randrange(0,11)
+    y1=random.randint(0,10)
+    x2=random.randint(0,10)
+    y2=random.randint(0,10)
+    dis=math.sqrt(((x1-x2)**2)+((y1-y2)**2))
+    print(round(dis,3))
+    x3 = int(input("Enter x3:"))
+    y3 = int(input("Enter y3:"))
+    while((x3>=-10)and(x3<=0)and(y3>=-10)and(y3<=0)):
+            break
+    else:
+            x3 = int(input("Enter x3:"))
+            y3 = int(input("Enter y3:"))
